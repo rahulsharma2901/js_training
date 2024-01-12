0dc7b052b0b5239cde6f4922d092701bd37cacb6
rahulsharma2901 <rahul.s@applocum.org>
2024-01-12 20:28:24 +0530

    examples of different methods of logical operator

---
+++ Chapter-5/logical.js
@@ -0,0 +1,16 @@
+x = "\n";
+
+let a = 12;
+let b = 13;
+let c = 15;
+console.log(a < b && b < c);
+console.log(x);
+
+let p = 17;
+let q = 12;
+let r = 13;
+console.log(p > q || q > r);
+console.log(x);
+
+let l = true;
+console.log(!l);

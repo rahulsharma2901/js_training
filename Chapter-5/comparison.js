71da0bc478f096534e23f331566e37f5308adc2d
rahulsharma2901 <rahul.s@applocum.org>
2024-01-12 20:21:20 +0530

    examples of different methods of comparison operator

---
+++ Chapter-5/comparison.js
@@ -0,0 +1,40 @@
+x = "\n";
+
+let a = 5;
+let b = "5";
+console.log(`${a} and "${b}" gives:`, a==b)
+console.log(x);
+
+let c = 4;
+let d = 5;
+console.log(`${c} and ${d} gives:`, c!=d)
+console.log(x);
+
+let e = 3;
+let f = "3";
+console.log(`${e} and "${f}" gives:`, e === f)
+console.log(x);
+
+let g = 12;
+let h = 13;
+console.log(`${g} and ${h} gives:`, g>h);
+console.log(x);
+
+let i = 13;
+let k = 15;
+console.log(`${k} and ${i} gives:`, k>=i);
+console.log(x);
+
+let l = 14;
+let m = 15;
+console.log(`${l} and ${m} gives:`, l<m);
+console.log(x);
+
+let n = 12;
+let o = 16;
+console.log(`${n} and ${o} gives:`, n<=o);
+console.log(x);
+
+let p = 17;
+let q = "17";
+console.log(`${p} and "${q}" gives:`, p!==q);

05d814e137a0143def5b74a4ef442a7966043e49
rahulsharma2901 <rahul.s@applocum.org>
2024-01-12 20:03:55 +0530

    examples of different methods of map

---
+++ Chapter-5/assignment.js
@@ -0,0 +1,65 @@
+a = "\n";
+
+let x = 10;
+console.log(x);
+console.log(a);
+
+let y = 7;
+y += 4;
+console.log(y);
+console.log(a);
+
+let z = 20;
+z -= 4;
+console.log(z);
+console.log(a);
+
+let b = 10;
+b *= 2;
+console.log(b);
+console.log(a);
+
+let c = 30;
+c /= 3;
+console.log(c);
+console.log(a);
+
+let d = 20;
+d %= 3;
+console.log(d);
+console.log(a);
+
+let e = 3;
+e **= 3;
+console.log(e);
+console.log(a);
+
+let f = 6;
+f <<= 1; //gives the binary answer shifted to the left
+console.log(f);
+console.log(a);
+
+let g = 5;
+g >>= 2;//gives the binary answer shifted to the right
+console.log(g)
+console.log(a)
+
+let h = 8;
+h >>>= 1;//gives the binary answer shifted to the right while removing the sign of the operand
+console.log(h);
+console.log(a);
+
+let i = 8;
+i &= 3;
+console.log(i);
+console.log(a);
+
+let j = 12;
+j ^= 3;
+console.log(j);
+console.log(a);
+
+let k = 10;
+k |= 3;
+console.log(k);
+console.log(a);

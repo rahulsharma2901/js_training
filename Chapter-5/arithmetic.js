aa73c58c6261d83304cd324ebf26d31bae4351ac
rahulsharma2901 <rahul.s@applocum.org>
2024-01-15 11:03:48 +0530

    examples of different methods of arithmetic operator

---
+++ Chapter-5/arithmetic.js
@@ -0,0 +1,55 @@
+x = "\n"
+
+let a = 4
+let b = 6
+sum = a + b;
+console.log(sum);
+console.log(x);
+
+let c = 7
+let d = 9
+diff = d - c
+console.log(diff);
+console.log(x);
+
+let e = 12
+let f = 12
+prod = e * f
+console.log(prod);
+console.log(x);
+
+let g = 16
+let h = 4
+div = g / h
+console.log(div);
+console.log(x);
+
+let i = 17
+let j = 4
+rem = i % j
+console.log(rem);
+console.log(x);
+
+let k = 5
+let l = 3
+exp = k ** l
+console.log(exp)
+console.log(x)
+
+let m = 13
+let n = 15
+inc = ++m
+dec = --n
+console.log(m)
+console.log(n)
+console.log(x)
+
+let o = 16
+neg = -o
+console.log(neg)
+console.log(x)
+
+let p = -273
+plus = +p
+console.log(p)
+console.log(x)

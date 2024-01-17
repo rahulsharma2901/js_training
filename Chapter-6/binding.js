ee069a078d7a6ba49d3f3002fe99e9f591a6f1ae
rahulsharma2901 <rahul.s@applocum.org>
2024-01-17 19:48:34 +0530

    examples of binding with this keyword

---
+++ Chapter-6/binding.js
@@ -0,0 +1,39 @@
+a = "\n"
+
+//Call binding
+const person1 = {
+    name: "Rahul",
+    introduce: function(age) {
+        console.log(`Hi, I'm ${this.name} and I'm ${age} years old.`);
+    }
+}
+const person2 = {
+    name: "Priyal",
+}
+person1.introduce.call(person2, 21);
+console.log(a);
+
+//Apply binding
+function displayInfo(greeting, farewell) {
+    console.log(`${greeting}, ${this.naming}!`);
+    console.log(`${farewell}, ${this.naming}!`);
+}
+const person = {
+    naming: "Sneha",
+}
+const greetings = ["Welcome", "Goodbye"];
+displayInfo.apply(person, greetings)
+console.log(a);
+
+//Bind binding
+function calculateTotal(price, tax) {
+    const total = price + (price * tax);
+    console.log((`Total cost of the laptop in ${this.currency} is: ${total}`));
+}
+const  product = {
+    tech: "Laptop",
+    price: 50000,
+    currency: "Rupees",
+}
+const calculateTotalForProduct = calculateTotal.bind(product);
+calculateTotalForProduct(product.price, 0.3);

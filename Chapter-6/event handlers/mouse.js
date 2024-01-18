59b9362e3b52cb38c1090d139df9f7b9fbde1f45
rahulsharma2901 <rahul.s@applocum.org>
2024-01-18 17:56:08 +0530

    Event handling examples of mouse events

---
+++ Chapter-6/event handlers/mouse.js
@@ -0,0 +1,36 @@
+//click
+const button1 = document.getElementById('button1');
+button1.addEventListener('click', function() {
+    this.textContent = "Clicked!";
+    this.style.backgroundColor = "Red";
+});
+
+//mouseover
+const button2 =document.getElementById('button2');
+button2.addEventListener('mouseover', function() {
+    this.style.backgroundColor = "rgb(0,150,255)";
+});
+
+//mouseout
+const button3 = document.getElementById('button3');
+button3.addEventListener('mouseout', function() {
+    this.style.backgroundColor = "Yellow";
+})
+
+//mousedown
+const button4 = document.getElementById('button4');
+button4.addEventListener('mouseup', function() {
+    this.style.backgroundColor = "Pink";
+});
+
+//mouseup
+const button5 = document.getElementById('button5');
+button5.addEventListener('mousedown', function() {
+    this.style.backgroundColor = "Green";
+});
+
+//mousemove
+const button6 = document.getElementById('button6');
+button6.addEventListener('mousemove', function() {
+    this.style.color = "Purple";
+})
